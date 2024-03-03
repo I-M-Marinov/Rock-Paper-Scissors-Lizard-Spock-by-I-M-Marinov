ede9ed7e1c3cef653e099be7fed562ca1e02e8a8
I-M-Marinov <lcfrrr@gmail.com>
2024-03-03 22:02:46 +0200

    comit2
    comit2

--- obj/Debug/net6.0/Rock-Paper-Scissors-Lizard-Spock-by-I-M-Marinov.AssemblyInfo.cs
+++ obj/Debug/net6.0/Rock-Paper-Scissors-Lizard-Spock-by-I-M-Marinov.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Rock-Paper-Scissors-Lizard-Spock-by-I-M-Marinov")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3d89ec05a8099cc8f86919f137654e07297d09d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Rock-Paper-Scissors-Lizard-Spock-by-I-M-Marinov")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Rock-Paper-Scissors-Lizard-Spock-by-I-M-Marinov")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
